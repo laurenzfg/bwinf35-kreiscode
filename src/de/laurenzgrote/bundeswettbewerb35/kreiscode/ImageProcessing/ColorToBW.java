4221de997480bc050b52efda6ea747bf598890c7
laurirbauri <lau_fi@t-online.de>
2017-04-18 18:13:31 +0200

    Refactoring

--- src/de/laurenzgrote/bundeswettbewerb35/kreiscode/ImageProcessing/ColorToBW.java
+++ src/de/laurenzgrote/bundeswettbewerb35/kreiscode/ImageProcessing/ColorToBW.java
@@ -1,27 +1,29 @@
 package de.laurenzgrote.bundeswettbewerb35.kreiscode.ImageProcessing;
 
+import de.laurenzgrote.bundeswettbewerb35.kreiscode.Coordinate;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.Arrays;
+import java.util.Stack;
 
 public class ColorToBW {
-    // Buntes Bild
-    private BufferedImage rgbImage;
     private int width, height;
 
-    private double avgBrightness = 0.0; // Wird später erfasst
+    // Binärbild
+    private boolean[][] swImage;
+
+    // Durchschnittswerte
+    private double avgBrightness = 0.0;
+    private double avgScharr = 0.0;
 
     public static boolean[][] colorToBW (BufferedImage rgbImage) {
-        return new ColorToBW(rgbImage).generateSW();
+        return new ColorToBW(rgbImage).getSWImage();
     }
 
     private ColorToBW(BufferedImage rgbImage) {
-        this.rgbImage = rgbImage;
         width = rgbImage.getWidth();
         height = rgbImage.getHeight();
-    }
 
-    private boolean[][] generateSW () {
         // Helligkeitswert für jeden Pixel berechnen
         double[][] brightness = new double[width][height];
 
@@ -33,103 +35,142 @@ public class ColorToBW {
                 int r = color.getRed();
                 int g = color.getGreen();
                 int b = color.getBlue();
-                // Helligkeit auf einer linearen Skala. Formel:
+
+                // Helligkeit auf einer linearen Skala bestimmen. Formel:
                 // https://en.wikipedia.org/wiki/Grayscale#Colorimetric_.28luminance-preserving.29_conversion_to_grayscale
                 double lineaerLuminance = r*0.2126 + g*0.7152 + b*0.0722;
 
-                // Helligkeit abspeichern
+                // Helligkeitswert abspeichern
                 brightness[x][y] = lineaerLuminance;
                 avgBrightness += lineaerLuminance; // Counter für arithm. Mittel
             }
         }
-        // Mittel berechnen
+        // Arithm. Mittel der Helligkeit berechnen
         avgBrightness /= width*height;
 
-        // Entrauschen durch Median
-        double[][] medianFilteredImage = medianFilter(brightness);
-        // Kantenerkennung und ausfüllen der Flächen
-        boolean[][] scharrImage = scharrFilter(medianFilteredImage);
+        // Entrauschen durch Gauß-Filter
+        double[][] gaussImage = gaussianFilter(brightness);
+        // Kantenerkennung
+        double[][] scharrImage = scharrFilter(gaussImage);
 
-        return scharrImage;
+        // Speichern des Binärbildes
+        swImage = toBinary(scharrImage, gaussImage);
+    }
+
+    private boolean[][] getSWImage() {
+        return swImage;
     }
 
     /**
-     * Schärt das Bild mit einem Medianfilter. https://en.wikipedia.org/wiki/Median_filter
+     * Weichzeichnen mit Gauß: http://homepages.inf.ed.ac.uk/rbf/HIPR2/gsmooth.htm
      * @param in Ausgangsbild
-     * @return geschärftes Bild
+     * @return gegaußtetes Bild
      */
-    private double[][] medianFilter(double[][] in) {
-        // Gewichtung des Pixels und seiner Umgebung
-        final double mask[] =  {1.0, 1.0, 1.0, 1.0, 1.0,
-                                1.0, 2.0, 3.0, 2.0, 1.0,
-                                1.0, 3.0, 4.0, 3.0, 1.0,
-                                1.0, 2.0, 3.0, 2.0, 1.0,
-                                1.0, 1.0, 1.0, 1.0, 1.0};
-        // Ausgabe der Funktion
-        double[][] medianFilteredImage = new double[width][height];
+    private double[][] gaussianFilter(double[][] in) {
+        // Gaßsche Normalverteilung. Sigma: 5
+        // Berechnungstool: http://dev.theomader.com/gaussian-kernel-calculator/
+        final double kernel[] =  {0.192077,0.203914,0.208019,0.203914,0.192077};
+        // Abstand, der zur Seite zur Berechnung benötigt wird
+        final int kernelGap = (kernel.length - 1) / 2; // ==2
 
-        // Randreihe wird nicht beachtet, damit kein OutOfBounds entsteht
-        for (int x = 2; x < width - 2; x++) {
-            for (int y = 2; y < height - 2; y++) {
-                // Werte für Median
-                double medianArray[] = new double[40];
-                // Position in der 1D-Maske
-                int maskPos = 0;
-                // Für jedes Pixel um das Pixel (x, y):
-                for (int x1 = x - 2; x1 <= x + 2; x1++) {
-                    for (int y1 = y - 2; y1 <= y + 2; y1++) {
-                        // Sooft in die Liste wie die maske bestimmt!
-                        double multiplier = mask[maskPos];
-                        for (int i = 0; i < multiplier; i++) {
-                            medianArray[maskPos + i] = in[x][y];
-                        }
-                        maskPos++;
-                    }
+        // Anwendung des Filters in horizontale Richung:
+        double[][] hGauss = new double[width][height];
+
+        for (int x = 0; x < width; x++) {
+            // An den Rändern Platz lassen, da dort der Kernel nicht "hineinpasst"
+            for (int y = kernelGap; y < height - kernelGap; y++) {
+                double newVal = 0.0;    // Neuer Grauwert nach Weichzeichnung
+                int i = 0;              // Counter für Position im Kernel
+                // Einfügen der umliegenden Felder nach Gewicht
+                for (int y1 = y - kernelGap; y1 <= y + kernelGap; y1++) {
+                    newVal += in[x][y1] * kernel[i];
+                    i++;
                 }
-
-                // Sortieren und Median in die Ausgabe schreiben
-                Arrays.sort(medianArray);
-                medianFilteredImage[x][y] = medianArray[19]; // 20 -> 19 (Nullindizierung)
+                // Speichern des neuen Grauwertes
+                hGauss[x][y] = newVal;
             }
         }
 
-        return medianFilteredImage;
+        // Anwendung des Filters in vertikale Richung:
+        double[][] vGauss = new double[width][height];
+
+        for (int y = 0; y < height; y++) {
+            // An den Rändern Platz lassen, da dort der Kernel nicht "hineinpasst"
+            for (int x = kernelGap; x < width - kernelGap; x++) {
+                double newVal = 0.0;    // Neuer Grauwert nach Weichzeichnung
+                int i = 0;              // Counter für Position im Kernel
+                // Ausrechnen der Kerne mit Convulution (http://homepages.inf.ed.ac.uk/rbf/HIPR2/convolve.htm)
+                for (int x1 = x - kernelGap; x1 <= x + kernelGap; x1++) {
+                    newVal += hGauss[x1][y] * kernel[i]; // Einfügen des Wertes
+                    i++;                                 // Nächste Position im Kernel
+                }
+                // Speichern des neuen Grauwertes
+                vGauss[x][y] = newVal;
+            }
+        }
+
+        // Randwerte sind nicht korrekt gemittelt worden, dort wird die Ausgangsfarbe eingefügt
+        for (int y = 0; y < height; y++) {
+            // Obere/Untere Zeilen oder nur die Bande Rechts und Links
+            if (y < kernelGap || y >= height - kernelGap) {
+                // Oben/Unten
+                for (int x = 0; x < width; x++)
+                    vGauss[x][y] = in[x][y];
+            } else {
+                // Rechts + Links
+                for (int x = 0; y < kernelGap; x++)
+                    vGauss[x][y] = in[x][y];
+                for (int x = width - kernelGap; x < width; x++)
+                    vGauss[x][y] = in[x][y];
+            }
+        }
+
+        return vGauss;
     }
 
 
     /**
-     * http://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/sobel_derivatives/sobel_derivatives.html
+     * Berechnung der Ableitung über den Bildkontrast mit dem Scharr-Operator
+     * http://homepages.inf.ed.ac.uk/rbf/HIPR2/sobel.htm
      * @param in
      * @return Ergebnis des Sobel-Operators
      */
-    private boolean[][] scharrFilter(double[][] in) {
-        // Scharr-Masken
-        final double[] vertMask     = {-3.0, -10.0, -3.0,
-                                        0.0, 0.0  , 0.0,
-                                        3.0, 10.0 , 3.0};
-        final double[] horizMask    = {-3.0, 0.0 , 3.0,
-                                       -10.0,0.0 ,10.0,
-                                       -3.0, 0.0 , 3.0};
+    private double[][] scharrFilter(double[][] in) {
+        // Scharr-Kernel
+        // (Die .ac.uk-Seite verwendet den älteren Sobel-Operator,
+        //  der Scharr-Operator ist aber bei kleinen Kerneln besser:
+        //  http://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/sobel_derivatives/sobel_derivatives.html#
 
-        // Ableitungsfunktionen des Graphens in beide Richtungen
+        // Kernel für vertikale Ableitung:
+        final double[] vertKernel = {-3.0, -10.0, -3.0,
+                0.0, 0.0, 0.0,
+                3.0, 10.0, 3.0};
+        // Kernel für horizontale Ableitung:
+        final double[] horizKernel = {-3.0, 0.0, 3.0,
+                -10.0, 0.0, 10.0,
+                -3.0, 0.0, 3.0};
+        // Abstand, der zur Seite zur Berechnung benötigt wird
+        final int kernelGap = (int) ((Math.sqrt(vertKernel.length) - 1) / 2); // ==1
+
+        // Ergebniss d. Ableitungsfunktionen des Graphens in beide Richtungen
         double[][] vertScharr = new double[width][height];
         double[][] horizScharr = new double[width][height];
 
-        for (int x = 2; x < width - 2; x++) {
-            for (int y = 2; y < height - 2; y++) {
-                int maskPos = 0; // Pos. in 1D-Maske
-                double vert =  0.0; // Werte der Ableitung
-                double horiz = 0.0;
-                // Umliegende Pixel um (x, y)
+        for (int x = kernelGap; x < width - kernelGap; x++) {
+            for (int y = kernelGap; y < height - kernelGap; y++) {
+                int maskPos = 0;    // Counter für Position im Kernel
+                double vert = 0.0; // Vertikaler Ableitungswert
+                double horiz = 0.0; // Horizontaler Ableitungswert
+                // Ausrechnen der Kerne mit Convulution (http://homepages.inf.ed.ac.uk/rbf/HIPR2/convolve.htm)
                 for (int x1 = x - 1; x1 <= x + 1; x1++) {
                     for (int y1 = y - 1; y1 <= y + 1; y1++) {
-                        // Laden der relevanten Multiplier
-                        double vertMultiplier  = vertMask[maskPos];
-                        double horizMultiplier = horizMask[maskPos];
-                        // Und enstprechen hinaufrechnen
-                        vert  += in[x1][y1] * vertMultiplier;
+                        // Laden der relevanten Multiplikatoren
+                        double vertMultiplier = vertKernel[maskPos];
+                        double horizMultiplier = horizKernel[maskPos];
+                        // Einfügen der entsprechenden Werte
+                        vert += in[x1][y1] * vertMultiplier;
                         horiz += in[x1][y1] * horizMultiplier;
-                        maskPos++;
+                        maskPos++; // Nächste Position im Kernel
                     }
                 }
                 // Speichern der berechneten Werte
@@ -140,63 +181,86 @@ public class ColorToBW {
 
         // Vereinigen der beiden Ableitungen
         double[][] combScharr = new double[width][height];
-        double avgScharr = 0.0;
-        for (int x = 2; x < width - 2; x++) {
-            for (int y = 2; y < height - 2; y++) {
+        for (int x = kernelGap; x < width - kernelGap; x++) {
+            for (int y = kernelGap; y < height - kernelGap; y++) {
                 // Laden der beiden Ableitungswerte zu (x, y)
-                double vert  = vertScharr[x][y];
+                double vert = vertScharr[x][y];
                 double horiz = horizScharr[x][y];
                 // Formel
                 double val = Math.sqrt(vert * vert + horiz * horiz);
-                // Speichern in Variable und Durchschnittscounter
+                // Speichern
                 combScharr[x][y] = val;
                 avgScharr += val;
             }
         }
-        avgScharr /= width*height; // Mitteln
+        avgScharr /= (width - 2 * kernelGap) * (height - 2 * kernelGap); // Mitteln
 
-        boolean[][] binaryImage = new boolean[width][height];
-        double treshhold = Math.max(75, 1.5*avgScharr);
-        // Am Rand sind "komische" Werte
-        for (int x = 3; x < width-3; x++) {
-            for (int y = 3; y < height - 3; y++) {
-                if (combScharr[x][y] > treshhold)
-                    binaryImage[x][y] = true;
+        // Auch hier gibts wieder unebachtete Randwerte
+        // Hier setze ich dann einen hohen Wert ein, damit im nächsen Schritt ein schwarzer Rand entsteht
+        for (int y = 0; y < height; y++) {
+            // Obere/Untere Zeilen oder nur die Bande Rechts und Links
+            if (y < kernelGap || y >= height - kernelGap) {
+                // Oben/Unten
+                for (int x = 0; x < width; x++)
+                    combScharr[x][y] = Integer.MAX_VALUE;
+            } else {
+                // Rechts + Links
+                for (int x = 0; y < kernelGap; x++)
+                    combScharr[x][y] = Integer.MAX_VALUE;
+                for (int x = width - kernelGap; x < width; x++)
+                    combScharr[x][y] = Integer.MAX_VALUE;
             }
         }
 
-        // Erweitern
-        /*for (int x = 2; x < width - 2; x++) {
-            for (int y = 2; y < height - 2; y++) {
-                if (binaryImage[x][y]) {
-                    double color = in[x][y];
+        return combScharr;
+    }
+    private boolean[][] toBinary(double[][] scharrImage, double[][] gaussImage) {
+        // Binärisieren mit Schwellwert
+        boolean[][] binaryImage = new boolean[width][height];
+        double treshhold = Math.max(200, 1.5*avgScharr); // Treshhold durch abschätzen ermittelt :D
+        Stack<Coordinate> hysteresisStack = new Stack<>();
 
-                    Stack<Coordinate> ffStack = new Stack<>();
-                    ffStack.push(new Coordinate(x - 1, y));
-                    ffStack.push(new Coordinate(x + 1, y));
-                    ffStack.push(new Coordinate(x, y - 1));
-                    ffStack.push(new Coordinate(x, y + 1));
-
-                    while(!ffStack.empty()) {
-                        Coordinate c = ffStack.pop();
-                        int x1 = c.getX();
-                        int y1 = c.getY();
-                        if (!binaryImage[x1][y1] && in[x1][y1] < avgBrightness && combScharr[x1][y1] < treshhold) {
-                            binaryImage[x1][y1] = true;
-
-                            if (x1 - 1 >= 0)
-                                ffStack.push(new Coordinate(x1 - 1, y1));
-                            if (x1 + 1 < width)
-                                ffStack.push(new Coordinate(x1 + 1, y1));
-                            if (y1 - 1 >= 0)
-                                ffStack.push(new Coordinate(x1, y1 - 1));
-                            if (y1 + 1 < height)
-                                ffStack.push(new Coordinate(x1, y1 + 1));
-                        }
-                    }
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (scharrImage[x][y] > treshhold) {
+                    binaryImage[x][y] = true;
+                    hysteresisStack.push(new Coordinate(x, y));
                 }
+
             }
-        }*/
+        }
+
+        // Hysterese mit Vervollständigung
+        treshhold = Math.max(100, avgScharr); // Treshhold durch abschätzen ermittelt :D
+        while (!hysteresisStack.empty()) {
+            Coordinate c = hysteresisStack.pop();
+            int x = c.getX(); int y = c.getY();
+
+            // Links
+            x -= 1;
+            if (x >= 0 && !binaryImage[x][y] && scharrImage[x][y] > treshhold) {
+                binaryImage[x][y] = true;
+                hysteresisStack.push(new Coordinate(x, y));
+            }
+            // Rechts
+            x += 2;
+            if (x < width && !binaryImage[x][y] && scharrImage[x][y] > treshhold) {
+                binaryImage[x][y] = true;
+                hysteresisStack.push(new Coordinate(x, y));
+            }
+            // Oben
+            x -= 1; y -= 1;
+            if (y >= 0 && !binaryImage[x][y] && scharrImage[x][y] > treshhold) {
+                binaryImage[x][y] = true;
+                hysteresisStack.push(new Coordinate(x, y));
+            }
+            // Unten
+            y += 2;
+            if (y < height && !binaryImage[x][y] && scharrImage[x][y] > treshhold) {
+                binaryImage[x][y] = true;
+                hysteresisStack.push(new Coordinate(x, y));
+            }
+        }
 
         return binaryImage;
     }
